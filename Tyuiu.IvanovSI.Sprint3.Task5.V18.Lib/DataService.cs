fff63824fb923209de3277ed4f697a4bcecb21c4
Семён Иванов <lilepopendr1@mail.ru>
2023-10-24 22:13:05 +0500

    Task5

---
+++ Tyuiu.IvanovSI.Sprint3.Task5.V18.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.IvanovSI.Sprint3.Task5.V18.Lib
+{
+    public class DataService : ISprint3Task5V18
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeries = sumSeries + ((Math.Cos(x)) + (Math.Pow(j, 2) / 2));
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
